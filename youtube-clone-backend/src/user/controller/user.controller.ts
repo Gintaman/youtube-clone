3194826eab5da96056707d56d42f70d46e2a6cc6
Jeffrey Chen <mechazawa@pop-os.localdomain>
2024-07-06 22:19:02 -0700

    init

---
+++ youtube-clone-backend/src/user/controller/user.controller.ts
@@ -0,0 +1,42 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+} from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { User } from '../models/user.interface';
+import { Observable } from 'rxjs';
+
+@Controller('users')
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post()
+    create(@Body() user: User): Observable<User> {
+        return this.userService.create(user);
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id): Observable<User> {
+        return this.userService.findOne(id);
+    }
+
+    @Get()
+    findAll(): Observable<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id') id): Observable<User> {
+        return this.userService.deleteOne(Number(id));
+    }
+
+    @Put(':id')
+    updateOne(@Param('id') id: string, @Body() user: User) {
+        return this.userService.updateOne(Number(id), user);
+    }
+}
---
+++ youtube-clone-backend/src/user/models/user.entity.ts
@@ -0,0 +1,14 @@
+// Database entity definition
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ unique: true })
+    username: string;
+}
